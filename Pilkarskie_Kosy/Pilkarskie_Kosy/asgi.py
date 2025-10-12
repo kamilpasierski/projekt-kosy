71878e172c54eb0fabc755fd8ed77b4e9af13be3
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-10-12 10:15:45 +0200

    Creating project template

---
+++ Pilkarskie_Kosy/Pilkarskie_Kosy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Pilkarskie_Kosy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pilkarskie_Kosy.settings')
+
+application = get_asgi_application()
