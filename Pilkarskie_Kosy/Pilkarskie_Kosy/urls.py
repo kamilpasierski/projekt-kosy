f2c1ff2d917a062774982f5167606a3144001748
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-10-14 20:44:46 +0200

    Created api for local users

--- Pilkarskie_Kosy/Pilkarskie_Kosy/urls.py
+++ Pilkarskie_Kosy/Pilkarskie_Kosy/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api_users/', include('api_users.urls')),
     path('', include('test_data.urls')),
 ]
