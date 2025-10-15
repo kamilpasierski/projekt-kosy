291752f308d1016a2bc5a093c9957cf9656d5109
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-10-15 19:18:06 +0200

    update

--- Pilkarskie_Kosy/api_users/tests.py
+++ Pilkarskie_Kosy/api_users/tests.py
@@ -2,7 +2,7 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 
-class ArticleApiTest(APITestCase):
+class UserApiTest(APITestCase):
     def test_get_users(self):
         url = reverse('get-users')
         response = self.client.get(url)
