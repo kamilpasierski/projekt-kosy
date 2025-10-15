3b2e846a376f3e558dd110633f9391c7caedd612
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-10-15 19:16:13 +0200

    Register api updated

---
+++ Pilkarskie_Kosy/api_users/tests.py
@@ -0,0 +1,9 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+
+class ArticleApiTest(APITestCase):
+    def test_get_users(self):
+        url = reverse('get-users')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
