1711aa278ed50e82302221d86f8572d6745ec3df
Daniel321W <daniel.kozak@studenci.collegiumwitelona.pl>
2025-10-20 00:04:38 +0200

    Initial commit projektu kosy

--- Pilkarskie_Kosy/test_data/tests.py
+++ Pilkarskie_Kosy/test_data/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Item
 
-# Create your tests here.
+
+
+class ItemModelTest(TestCase):
+    def test_create_item(self):
+        
+        item = Item.objects.create(name="Piłka")
+        self.assertEqual(str(item), "Piłka")  
+        self.assertIsNotNone(item.created)    
+
+
+
+class IndexViewTest(TestCase):
+    def test_index_view_status_code(self):
+        
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_template_used(self):
+        
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
