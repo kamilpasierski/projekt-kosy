849cd7546a243641903fcca5b14e8df1fc00de13
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-10-26 12:42:49 +0100

    Add function hash password

--- Pilkarskie_Kosy/api_register/serializers.py
+++ Pilkarskie_Kosy/api_register/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -8,9 +9,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'password']
 
-        def create(self, validated_data):
-            user = User.objects.create_user(
-                username=validated_data['username'],
-                password=validated_data['password']
-            )
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
--- Pilkarskie_Kosy/api_register/views.py
+++ Pilkarskie_Kosy/api_register/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 @api_view(['POST'])
 def postData(request):
     serializer = RegisterSerializer(data=request.data)
+    print("Requested data:", request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
