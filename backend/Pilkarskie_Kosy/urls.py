536741ae441caa82abb8e8529ae2466aa4802591
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-11-06 22:33:59 +0100

    login function

--- backend/Pilkarskie_Kosy/urls.py
+++ backend/Pilkarskie_Kosy/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api_users/', include('api_users.urls')),
-    path('api_register/', include('api_register.urls'))
+    path('api/users/', include('api_users.urls')),
+    path('api/register/', include('api_register.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
