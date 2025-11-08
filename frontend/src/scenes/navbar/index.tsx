6488ca881f3aa20644e736885bde561c194a3bed
kamilpasierski <pasierskidev@gmail.com>
2025-11-08 13:18:34 +0100

    Added login page and moved useEffect logic to Navbar.tsx

--- frontend/src/scenes/navbar/index.tsx
+++ frontend/src/scenes/navbar/index.tsx
@@ -1,61 +1,58 @@
-import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link.tsx";
-import { SelectedPage } from "../../shared/types.ts";
 import useMediaQuery from "../../hooks/useMediaQuery.ts";
 import ActionButton from "../../shared/ActionButton.tsx";
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+        const flexBetween = "flex items-center justify-between";
+        const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+        const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+        const navigate = useNavigate();
+
+        const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+        useEffect(() => {
+            const handleScroll = () => {
+                if (window.scrollY === 0) {
+                    setIsTopOfPage(true);
+                } else {
+                    setIsTopOfPage(false);
+                }
+            };
+            window.addEventListener("scroll", handleScroll);
+            return () => window.removeEventListener("scroll", handleScroll);
+        }, []);
 
 
-type Props = {
-    isTopOfPage: boolean;
-    selectedPage: SelectedPage;
-    setSelectedPage: (value: SelectedPage) => void;
-};
+        const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
-const Navbar = ({  isTopOfPage, selectedPage, setSelectedPage} : Props) => {
-    const flexBetween = "flex items-center justify-between";
-    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
-    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
-    const navigate = useNavigate();
+        return <nav>
+            <div
+                className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+            >
+                <div className={`${flexBetween} mx-auto w-5/6`}>
+                    <div className={`${flexBetween} w-full gap-16`}>
 
-    return <nav>
-        <div
-            className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
-        >
-            <div className={`${flexBetween} mx-auto w-5/6`}>
-                <div className={`${flexBetween} w-full gap-16`}>
+                        {/* lewa strona */}
+                        <Link to="/"> <img alt={"logo"} src={Logo} /> </Link>
+                        {/* prawa strona */}
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexBetween} w-full`}>
+                                <div className={`${flexBetween} gap-8 text-sm`}>
 
-                    {/* lewa strona */}
-                    <img alt={"logo"} src={Logo} />
+                                    <Link to="/">Strona główna</Link>
+                                    <Link to="/mapa">Mapa</Link>
+                                    <Link to="/placeholder1">Placeholder</Link>
+                                    <Link to="/placeholder2">Placeholder</Link>
 
-                    {/* prawa strona */}
-                    {isAboveMediumScreens ? (
-                    <div className={`${flexBetween} w-full`}>
-                        <div className={`${flexBetween} gap-8 text-sm`}>
-                            <Link page="Home"
-                            selectedPage={selectedPage}
-                                  setSelectedPage={setSelectedPage}
-                            />
-                            <Link page="Map"
-                                  selectedPage={selectedPage}
-                                  setSelectedPage={setSelectedPage}
-                            />
-                            <Link page="About us"
-                                  selectedPage={selectedPage}
-                                  setSelectedPage={setSelectedPage}
-                            />
-                            <Link page="Credits"
-                                  selectedPage={selectedPage}
-                                  setSelectedPage={setSelectedPage}
-                            />
-                        </div>
-                        <div className={`${flexBetween} gap-8`}>
-                            <ActionButton onClick={() => navigate('register')}>Sign In</ActionButton>
-                        </div>
-                    </div>
+                                </div>
+                                <div className={`${flexBetween} gap-8`}>
+                                    <Link to="/login">Zaloguj się</Link>
+                                    <ActionButton onClick={() => navigate('register')}>Zarejestruj się</ActionButton>
+                                </div>
+                            </div>
                         ) : (
                             <button
                                 className="rounded-full bg-secondary-500 p-2"
@@ -64,41 +61,32 @@ const Navbar = ({  isTopOfPage, selectedPage, setSelectedPage} : Props) => {
                                 <Bars3Icon className="h-6 w-6 text-white"/>
                             </button>
                         )}
+                    </div>
                 </div>
             </div>
-        </div>
-        {/* menu mobilne */}
-        {!isAboveMediumScreens && isMenuToggled && (
-            <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
-                {/* ikonka zamykania */}
-                <div className="flex justify-end p-12">
-                    <button onClick={() => setIsMenuToggled (!isMenuToggled)}>
-                        <XMarkIcon className="h-6 w-6 text-gray-400" />
-                    </button>
-                </div>
-                {/* zawartość menu */}
-                <div className="ml-[33%] flex flex-col gap-10 text-2xl">
-                <Link page="Home"
-                      selectedPage={selectedPage}
-                      setSelectedPage={setSelectedPage}
-                />
-                <Link page="Map"
-                      selectedPage={selectedPage}
-                      setSelectedPage={setSelectedPage}
-                />
-                <Link page="About us"
-                      selectedPage={selectedPage}
-                      setSelectedPage={setSelectedPage}
-                />
-                <Link page="Credits"
-                      selectedPage={selectedPage}
-                      setSelectedPage={setSelectedPage}
-                />
-                </div>
-            </div>
-        )}
-    </nav>
-};
+            {/* menu mobilne */}
+            {!isAboveMediumScreens && isMenuToggled && (
+                <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+                    {/* ikonka zamykania */}
+                    <div className="flex justify-end p-12">
+                        <button onClick={() => setIsMenuToggled (!isMenuToggled)}>
+                            <XMarkIcon className="h-6 w-6 text-gray-400" />
+                        </button>
+                    </div>
+                    {/* zawartość menu mobilnego */}
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+
+                        <Link to="/" onClick={() => setIsMenuToggled(!isMenuToggled)}>Strona główna</Link>
+                        <Link to="/mapa" onClick={() => setIsMenuToggled(!isMenuToggled)}>Mapa</Link>
+                        <Link to="/placeholder" onClick={() => setIsMenuToggled(!isMenuToggled)}>placeholder</Link>
+                        <Link to="/placeholder" onClick={() => setIsMenuToggled(!isMenuToggled)}>placeholder</Link>
 
 
-export default Navbar
+                    </div>
+                </div>
+            )}
+        </nav>
+    };
+
+
+    export default Navbar;
