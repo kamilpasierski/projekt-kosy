e187aec4016ed64b2b8ee8244ffb158c8e99e0cb
Daniel321W <daniel.kozak@studenci.collegiumwitelona.pl>
2025-11-11 16:44:33 +0100

    Test api_login

--- backend/Pilkarskie_Kosy/settings.py
+++ backend/Pilkarskie_Kosy/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
 
     'rest_framework_simplejwt',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders'
 ]
 
--- backend/Pilkarskie_Kosy/urls.py
+++ backend/Pilkarskie_Kosy/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('api/register/', include('api_register.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api_login/', include('api_login.urls')),
 ]
