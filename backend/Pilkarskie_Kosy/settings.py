c195fb76b4231bd3aa9dc201458fb0bacc543863
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-11-19 11:08:10 +0100

    email register (working on)

--- backend/Pilkarskie_Kosy/settings.py
+++ backend/Pilkarskie_Kosy/settings.py
@@ -12,10 +12,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'mail-serwer329971.lh.pl'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
