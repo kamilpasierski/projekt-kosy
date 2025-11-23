7d684ba705ea3abcfef31286da13f0c1e6db6963
kamilpasierski <pasierskidev@gmail.com>
2025-11-23 14:28:15 +0100

    fix fix fix fix

--- backend/Pilkarskie_Kosy/settings.py
+++ backend/Pilkarskie_Kosy/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'api_register',
     'rest_framework_simplejwt',
     'rest_framework',
     'rest_framework.authtoken',
--- backend/api_register/urls.py
+++ backend/api_register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register_user, name='register_post')
+    path('', views.register_user, name='register_post'),
+    path('activate/<uidb64>/<token>', views.activate_account, name='activate')
 ]
