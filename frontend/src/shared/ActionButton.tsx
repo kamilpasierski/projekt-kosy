c44ddc82e852b5e4b0e5a5f796cd653dc58c5adc
kamilpasierski <pasierskidev@gmail.com>
2025-11-26 17:42:36 +0100

    ActionButton.tsx styles change

--- frontend/src/shared/ActionButton.tsx
+++ frontend/src/shared/ActionButton.tsx
@@ -10,7 +10,7 @@ type Props = {
 const ActionButton = ({ children, onClick, type = "button", disabled = false }: Props) => {
     return (
         <button
-            className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+            className="bg-green-600 hover:bg-green-700 text-white px-5 py-2 rounded-full font-semibold text-sm lg:text-base transition-colors shadow-lg whitespace-nowrap cursor-pointer"
             onClick={onClick}
             type={type}
             disabled={disabled}
