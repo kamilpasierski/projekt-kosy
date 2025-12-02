1f220125d6300b062081924808a48f90f6287e0a
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-12-02 18:47:56 +0100

    password reset

--- backend/reset_password/urls.py
+++ backend/reset_password/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import password_reset_request, password_reset_confirm
+from .views import password_reset_request, password_reset_confirm
 
 urlpatterns = [
     path("", password_reset_request),
