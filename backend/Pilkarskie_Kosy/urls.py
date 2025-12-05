2ea946c298b610e32fc8c801a4df01323c06b15a
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-12-05 21:19:44 +0100

    Add static path

--- backend/Pilkarskie_Kosy/urls.py
+++ backend/Pilkarskie_Kosy/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -15,3 +16,7 @@ urlpatterns = [
     path("password-reset/", include('reset_password.urls')),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
