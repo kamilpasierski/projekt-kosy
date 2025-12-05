9e19a1f3928ddcbcfa5d84cef2fe3973d6d44b40
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2025-12-05 21:19:10 +0100

    Add pillow lib

--- backend/models/models.py
+++ backend/models/models.py
@@ -5,6 +5,7 @@ User = get_user_model()
 
 class Club(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    path_image = models.ImageField(upload_to='clubs/', null=True, blank=True)
 
     def __str__(self):
         return self.name
