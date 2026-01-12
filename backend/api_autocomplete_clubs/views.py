128eadd835d6af4a47b679a2245c5e134b240732
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-01-12 20:38:27 +0100

    API autocomplete_clubs added

---
+++ backend/api_autocomplete_clubs/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models.models import Club
+
+class ClubAutocomplete(APIView):
+    def get(self, request):
+        query = request.GET.get("q", "")
+
+        clubs = (
+            Club.objects
+            .filter(name__icontains=query)
+            .order_by("name")[:10]
+        )
+
+        return Response([club.name for club in clubs])
