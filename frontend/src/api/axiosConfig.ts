e1cec08131fc39e06e71141c743815586a4d2f7c
kamilpasierski <pasierskidev@gmail.com>
2026-01-21 21:57:26 +0100

    update axios config file

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -9,7 +9,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('accessToken'); 
+        const token = localStorage.getItem('accessToken') || sessionStorage.getItem('accessToken'); 
         
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
