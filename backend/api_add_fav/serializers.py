33829d23b59936be0ac1c387ddf21f590c9d5f49
kamilpasierski <pasierskidev@gmail.com>
2026-01-23 17:59:26 +0100

    add date sorting and display creation date for followed clubs

--- backend/api_add_fav/serializers.py
+++ backend/api_add_fav/serializers.py
@@ -14,7 +14,7 @@ class UserClubSerializer(serializers.ModelSerializer):
 class FavoriteClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = FavoriteClub
-        fields = ['id', 'club']
+        fields = ['id', 'club', 'created_at']
 
     def create(self, validated_data):
         user = self.context['request'].user
