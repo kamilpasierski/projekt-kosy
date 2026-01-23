62007f4902b76a3d4fb9334ee1d9da6780c4bdd2
kamilpasierski <pasierskidev@gmail.com>
2026-01-23 18:18:41 +0100

    fix add_vaf urls path

--- backend/api_add_fav/urls.py
+++ backend/api_add_fav/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.SetUserClubView.as_view()),
-    path('watched_clubs', views.AddFavoriteClubAPIView.as_view()), 
+    path('watched_clubs/', views.AddFavoriteClubAPIView.as_view()), 
     path('watched_clubs/<int:pk>/', views.AddFavoriteClubAPIView.as_view()), 
 ]
